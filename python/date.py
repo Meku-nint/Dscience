5a51dfe50d82dc868a954862916d06395a5fc4e3
Meku-nint <mw908334@gmail.com>
2026-03-12 23:36:40 +0300

    date module

---
+++ python/date.py
@@ -0,0 +1,6 @@
+# date and time in python.
+import datetime
+x=datetime.datetime.now() # this will return the current date and time as a datetime object.
+print(x)
+y=datetime.datetime(2020, 5, 17) # this will create a datetime object representing May 17, 2020.
+print(y)
