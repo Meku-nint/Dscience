86194ccf41327f1f08a5cbf56302fa366c7032a9
Meku-nint <mw908334@gmail.com>
2026-03-12 21:43:36 +0300

    default parameter

--- python/function.py
+++ python/function.py
@@ -7,3 +7,9 @@ greeting =greet("Alice")
 print(greeting) # this will print "Hello, Alice!" which is the return value of the greet function when it is called with the argument "Alice"
 def pass_statement():
     pass # this is  a placeholder for a function that does nothing,it used for creating a function that is not yet implemented or for creating a function that is used as a placeholder for a future implementation.
+# parameter is a variable that is defined in the function definition and is used to receive the value passed to the function 
+# argument is the actual value that is passed to the function when the function is called.
+def my_function(country ="Egypt"):
+    print ("I am from " + country)
+my_function() # this will print "I am from Egypt" because the default value of the parameter country is "Egypt"
+my_function("USA") # this will print "I am from USA" because the argument "
