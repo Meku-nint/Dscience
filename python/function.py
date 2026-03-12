56e5fbc96ce2f41588ea872283c38d8e5dec1a72
Meku-nint <mw908334@gmail.com>
2026-03-12 21:46:29 +0300

    passing d/t data type

--- python/function.py
+++ python/function.py
@@ -13,3 +13,14 @@ def my_function(country ="Egypt"):
     print ("I am from " + country)
 my_function() # this will print "I am from Egypt" because the default value of the parameter country is "Egypt"
 my_function("USA") # this will print "I am from USA" because the argument "
+# passing d/t dataypes to a function is the process of providing values of different data types as arguments when calling a function. this allows the function to work with different types of data and perform operations based on the type of data passed.
+def print_info(name, age, city):
+    print("Name: ", name)
+    print("Age: ", age)
+    print("City: ", city)
+print_info("Alice", 30, "New York") # this will print the name, age, and city of the person passed as arguments to the function
+def list_info(fruits):
+    for fruit in fruits:
+        print (fruit)
+my_fruits = ["apple", "banana", "cherry"]
+list_info(my_fruits) # this will print the fruits in the list my_fruits which are passed as an argument to the function
