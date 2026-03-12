1a8a73cfbc9a73602e582c6fd33bd42f54652889
Meku-nint <mw908334@gmail.com>
2026-03-13 00:19:47 +0300

    commiteed

--- python/regex.py
+++ python/regex.py
@@ -1,1 +1,11 @@
-#RegEx ,or regular expression is a sequence 
+#RegEx ,or regular expression is a sequence characters that forms a search pattern.
+#RegEx can be used to check if a string contains the specified search pattern.
+#python has a built-in module called re to work with regular expressions.
+import re
+#search for a pattern in a string
+pattern = r"hello"
+string = "hello world"
+match = re.search(pattern, string)
+if match:
+    print("Pattern found in the string")
+else:    print("Pattern not found in the string")
