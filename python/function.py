8d1d814a41d6ba27aba927d8267b33900d9f16f7
Meku-nint <mw908334@gmail.com>
2026-03-12 21:29:27 +0300

    pass in python

--- python/function.py
+++ python/function.py
@@ -2,6 +2,8 @@
 # block of code which executes when it is called. it can be use for remove code repetition and make the code more organized and reusable.
 def greet (name):
     return "Hello, " +name + "!" 
-
+   # if the function have no return statement, it will return None by default.
 greeting =greet("Alice")
 print(greeting) # this will print "Hello, Alice!" which is the return value of the greet function when it is called with the argument "Alice"
+def pass_statement():
+    pass # this is  a placeholder for a function that does nothing,it used for creating a function that is not yet implemented or for creating a function that is used as a placeholder for a future implementation.
