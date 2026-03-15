58f85ce29ede75c2b42e44019aeabe21f1dfd5d2
Meku-nint <mw908334@gmail.com>
2026-03-15 15:27:12 +0300

    add choice method to get random value from array

--- numpy/random/randoma.py
+++ numpy/random/randoma.py
@@ -3,3 +3,8 @@
 from numpy import random
 x=random.randint(10)
 print(x) # this will print a random integer between 0 and 9 (inclusive)
+
+# generate random number form array we use choice method it takes array as an argument and it will return a random element from the array
+list_name=['john','michael','sarah','jessica']
+random_name=random.choice(list_name)
+print(random_name) # this will print a random name from the list_name array
