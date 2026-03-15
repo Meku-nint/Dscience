55df84891aeb3153640cab3dc0b32dfc5e07ff1e
Meku-nint <mw908334@gmail.com>
2026-03-15 15:04:55 +0300

    random number generating

---
+++ numpy/random/randoma.py
@@ -0,0 +1,5 @@
+# random number is generate based on the device time or os randomness or entropy
+
+from numpy import random
+x=random.randint(10)
+print(x) # this will print a random integer between 0 and 9 (inclusive)
