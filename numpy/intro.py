a7db9649cf142006bc59a42ab52455f74e94ce46
Meku-nint <mw908334@gmail.com>
2026-03-15 13:31:28 +0300

    accessing the 3D matrix

--- numpy/intro.py
+++ numpy/intro.py
@@ -17,3 +17,10 @@ print(one_dim_array.ndim)
 two_dim_array=np.array([[1,2,3],[4,5,6],[7,8,9]])
 print(two_dim_array)
 print(two_dim_array.ndim)
+
+# three dimensional array (tensor) an array with three axes. it is represented as a list of lists of lists in python. each inner list represents a 2D matrix.
+three_dim_array=np.array([[[1,2,3],[4,5,6],[7,8,9]],[[10,11,12],[13,14,15],[16,17,18]],[[19,20,21],[22,23,24],[25,26,27]]])
+print(three_dim_array)
+print(three_dim_array.ndim)
+print(three_dim_array[0,1,2]) # this means the first 2D matrix, second row, third column (0-based indexing)
+# 0 indicates the first 2D matrix, 1 indicates the second row, and 2 indicates the third column. so it will return the value 6.(so ,2Dmatrix,row,column)
