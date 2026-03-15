507c49c2d7bc560e6426110ff20259f899b9e3a0
Meku-nint <mw908334@gmail.com>
2026-03-15 11:54:49 +0300

    accessing array element

---
+++ numpy/access.py
@@ -0,0 +1,7 @@
+# accessing array elements in numpy is similar to accessing elements in a list, but with some additional features. you can use indexing and slicing to access specific elements or subarrays of a numpy array.
+import numpy as np
+
+# create a 2D array
+array= np.array([[1,2,3],[4,5,6],[7,8,9]])
+# access a single element using indexing
+print (array[0,0]) 
